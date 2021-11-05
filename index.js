de50e1b891d280024ced78f27fec70632c123d59
Michael Joyce <michael.joyce@illiniarts.com>
2021-11-05 07:19:47 -0500

    Menu (kinda) working

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
 const electron = require('electron');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow({});
     mainWindow.loadURL(`file://${__dirname}/main.html`);
+
+    const mainMenu = Menu.buildFromTemplate(menuTemplate);
+    Menu.setApplicationMenu(mainMenu);
 });
 
+const menuTemplate = [
+    {
+        label: 'File',
+        submenu: [
+            { label: 'New Todo' }
+        ]
+    },
+    {
+        label: 'Edit',
+        submenu: [
+            { label: 'Edit Todo' }
+        ]
+    }
+];
+
+if (process.platform === 'darwin') {
+    menuTemplate.unshift({label: 'Fake'});
+}
